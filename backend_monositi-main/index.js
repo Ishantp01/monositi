9d5a18e6ea53c5d9b280f7acc8e3d7e4aee95135
sparsh-15 <sparshsahu8435@gmail.com>
2025-10-06 00:37:14 +0530

    UPDATE CORS

--- backend_monositi-main/index.js
+++ backend_monositi-main/index.js
@@ -10,7 +10,7 @@ dotenv.config();
 const app = express();
 const corsOptions = {
   origin: "http://localhost:5173", // frontend URL
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
 };
 
