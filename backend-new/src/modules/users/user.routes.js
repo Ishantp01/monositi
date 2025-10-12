982f7952e836844941c93c880bb652c871de824a
Darkness1805 <139411743+Darkness1805@users.noreply.github.com>
2025-10-12 20:42:04 +0530

    d

--- backend-new/src/modules/users/user.routes.js
+++ backend-new/src/modules/users/user.routes.js
@@ -2,10 +2,11 @@ import express from 'express';
 const router = express.Router();
 
 
-import { updateUserProfile, sendOtp, verifyOtp } from './user.controller.js';
+import { updateUserProfile, sendOtp, verifyOtp ,registerUser} from './user.controller.js';
 import { protect } from '../../middlewares/authMiddleware.js';
 
 // Placeholder user routes - implement as needed
+router.post("/register", registerUser); 
 router.put('/update-profile', protect, updateUserProfile);
 router.post("/send-otp", sendOtp);
 router.post("/verify-otp", verifyOtp);
