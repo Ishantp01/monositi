5da0549c8471803f3a91b7b0f334a43d2fb62f6c
Darkness1805 <139411743+Darkness1805@users.noreply.github.com>
2025-10-14 09:54:01 +0530

    L

--- backend-new/src/modules/users/user.routes.js
+++ backend-new/src/modules/users/user.routes.js
@@ -1,7 +1,24 @@
-import express from 'express';
+
+@@ -2,67 +2,13 @@ import express from 'express';
 const router = express.Router();
 
 
+import { updateUserProfile, sendOtp, verifyOtp ,registerUser,getMyProfile,
+    updateMyProfile,
+    updateKyc,
+    updateContactPreferences,
+    requestRoleChange,
+    getAllUsers,
+    getUserById,
+    updateUserByAdmin,
+    deleteUser,
+    getMySubscription,
+    updateMySubscription,
+    getMyProperties,
+    getUserProperties} from './user.controller.js';
+import  upload  from '../../config/multer.js';
+
+import { protect,adminOnly } from '../../middlewares/authMiddleware.js';
 import { updateUserProfile, sendOtp, verifyOtp ,registerUser} from './user.controller.js';
 import { protect } from '../../middlewares/authMiddleware.js';
 
@@ -10,5 +27,6 @@ router.post("/register", registerUser);
 router.put('/update-profile', protect, updateUserProfile);
 router.post("/send-otp", sendOtp);
 router.post("/verify-otp", verifyOtp);
-
-export default router;
+router.route('/me')
+    .get(protect, getMyProfile)
+    .put(protect, updateMyProfile);
