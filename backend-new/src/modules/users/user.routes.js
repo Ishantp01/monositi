2e2bbd18df55ec2b7b8471e0ceefa9e2876e3646
Darkness1805 <139411743+Darkness1805@users.noreply.github.com>
2025-10-17 00:52:17 +0530

    make self admin api

--- backend-new/src/modules/users/user.routes.js
+++ backend-new/src/modules/users/user.routes.js
@@ -9,6 +9,7 @@ import { updateUserProfile, sendOtp, verifyOtp ,registerUser,getMyProfile,
     updateContactPreferences,
     requestRoleChange,
     getAllUsers,
+    makeAdmin,
     getUserById,
     updateUserByAdmin,
     deleteUser,
@@ -52,6 +53,9 @@ router.route('/:id')
     .get(protect, adminOnly, getUserById)
     .patch(protect, adminOnly, updateUserByAdmin)
     .delete(protect, adminOnly, deleteUser);
+
+// Promote a user to admin
+router.put('/:id/make-admin',  makeAdmin);
 
 //================================================================
 // 5. Subscription & Features Routes (Requires Authentication)
