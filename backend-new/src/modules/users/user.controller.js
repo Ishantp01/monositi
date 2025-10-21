41bdbabd214ac56d4dadbb8efa51fdb7039a47cf
Darkness1805 <139411743+Darkness1805@users.noreply.github.com>
2025-10-21 20:23:49 +0530

    Revert "n"
    This reverts commit 0d3fd2f5702ac6d89950eb301eb80a7f343cfcad.

--- backend-new/src/modules/users/user.controller.js
+++ backend-new/src/modules/users/user.controller.js
@@ -119,7 +119,7 @@ export const sendOtp = async (req, res) => {
         }, 120000);
 
         res.status(200).json({ success: true, message: "OTP sent successfully via WhatsApp" });
-        res.status(200).json({ success: true, message: `OTP sent successfully ${otp}`  });  
+        res.status(200).json({ success: true, message: "OTP sent successfully" });  
 
     } catch (err) {
         console.error("Send OTP error:", err);
